334b1f0190ebb9d28d9de261b596ed03035acc00
Francesco <francesco.mazzini432@gmail.com>
2022-10-02 23:39:30 +0200

    Added the logo and the subtitle
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import "../assets/css/styles.css";
+import "../assets/css/style.css";
 import "../assets/css/bootstrap.min.css";
 
 export default class Header extends Component {
     render () {
         return (
             
-            <div>
-                
+            <div className='container'>
+
+                <div className='row justify-content-center mt-5 pb-5'>
+
+                    <div className='col-8'>
+
+                        <img src="img/logo-sd-23.png" className = "main-logo"/>
+
+                    </div>
+                    
+                </div>
+
+                <div className='row justify-content-center mt-5'>
+
+                    <div className='col-6'>
+
+                        <h1 className='h5 font-weight-bold'>SAVE THE DATE</h1>
+                        <h1 className='h5 font-weight-bold'>FEBRUARY 2023</h1>
+                        
+                    </div>
+                    
+                </div>
+
             </div>
         )
     }
