47ea07485ffcafd1a2bffd5963692e1845ed9d96
Francesco <francesco.mazzini432@gmail.com>
2022-10-02 23:18:23 +0200

    Added header file
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../assets/css/styles.css";
+import "../assets/css/bootstrap.min.css";
+
+export default class Header extends Component {
+    render () {
+        return (
+            
+            <div>
+                
+            </div>
+        )
+    }
+}
