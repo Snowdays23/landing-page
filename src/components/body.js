84d0da544c734631aaf4aeb6604e5a93b3c0a163
Francesco <francesco.mazzini432@gmail.com>
2022-10-06 19:08:54 +0200

    Added picture and button and made some adjustments
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import "../assets/css/style.css";
 import "../assets/css/bootstrap.min.css";
+import "../assets/css/bootstrap-override.css";
+import "../assets/css/style.css";
 
 export default class Body extends Component {
     render () {
@@ -20,7 +21,29 @@ export default class Body extends Component {
 
                 <br/>
                     
+                
+                <div className='container mt-2'>
+
+                    <div className='row justify-content-center'>
+
+                        <div className='col-3'> 
+                        
+                            <button type="button" className="btn btn-primary p-3" id="learn-more-btn"> learn more </button>
+                        
+                        </div>
+
+                        <div className='col-7'> 
+
+                            <img src="img/pic-girl-snowboard.jpg" id="girl-snowboard"/>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
                 <img src="img/blue_stars.svg" id = "blue-stars-reversed"/>
+
 
             </div>
             
