0702c73240bdd055ca608b74799676020330cdf0
Francesco <francesco.mazzini432@gmail.com>
2022-10-16 15:23:20 +0200

    smth else
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/components/body.js
+++ src/components/body.js
@@ -33,10 +33,10 @@ export default class Body extends Component {
 
                         <div className='col-12'> 
 
-                        <iframe className = "yt-video"  src="https://www.youtube.com/embed/68TUBKxxVug?autoplay=1&mute=1"
-                         title="YouTube video player" frameborder="0" 
-                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                         allowfullscreen></iframe>
+                            <iframe className = "yt-video"  src="https://www.youtube.com/embed/68TUBKxxVug?autoplay=1&mute=1"
+                            title="YouTube video player" frameborder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                            allowfullscreen></iframe>
 
                         </div>
 
