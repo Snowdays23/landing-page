473f95d9643403cebc184c0f333505b2d3ae8bfe
Francesco <francesco.mazzini432@gmail.com>
2022-10-28 15:29:45 +0200

    Refactored Body2
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header'
 import Body1 from './components/body1'
-// import Body2 from './components/body2'
+import Body2 from './components/body2'
 // import Body3 from './components/body3'
 // import Body4 from './components/body4';
 // import "./assets/js/PureSnow.js";
@@ -15,8 +15,8 @@ function App() {
       </div>
       <Header></Header>
       <Body1></Body1>
-      {/* <Body2></Body2>
-      <Body3></Body3>
+      <Body2></Body2>
+      {/* <Body3></Body3>
       <Body4></Body4> */}
       
     </div>
--- src/components/body2.js
+++ src/components/body2.js
@@ -4,65 +4,44 @@ import pool from '../assets/img/previousImg/07-20.34__MG_5258.jpg';
 import whiteStars from '../assets/img/white_stars.svg';
 import "../assets/css/bootstrap.min.css";
 import "../assets/css/bootstrap-override.css";
-import "../assets/css/style.css";
+import "../assets/css/styleBody2.css";
+// import "../assets/css/style.css";
 import "../assets/js/Carousel.js";
 
 export default class Body2 extends Component {
     render () {
         return (
 
-            <div className = " wrapper-outside-body2 ">
+            <div className = "wrapper-outside-body relative offset-up-15 pb-15">
 
-                <div className='blue-bg  '></div>
+                <div className = "container">
 
-                <div className = "offset-up">
-
-                    <div className='row justify-content-center wrapper-title-sport' >
+                    <div className='row justify-content-center relative offset-up-5' >
 
                         <div className='col-10'>
 
-                            <h2 className='text-white font-josefin'>SPORTS & OPPORTUNITIES</h2>
+                            <h2 className='text-blue font-josefin title'>SPORTS AND OPPORTUNITIES</h2>
 
                         </div>
 
                     </div>              
 
                     
-                    <div className='row justify-content-center offset-down'>
+                    <div className='row justify-content-center'>
                         <div className='col-10'>
-                            <div id="carouselExampleControls" class="carousel">
-                                <div className='slide'>
-                                    <img name="carouselImage" src={skiGuy} class="slide" alt=""/>
-                                </div>
+                            <div className='slide'>
+                                <img name="carouselImage" src={skiGuy} alt=""/>
                             </div>
                         </div>
                     </div>
 
-                    <img src={whiteStars} id = "white-stars2"/>
-
-                    <div className='row justify-content-center offset-down2'>
+                    <div className='row justify-content-center mt-5 '>
                         <div className='col-10'>
-                            <div id="carouselExampleControls" class="carousel">
-                                <div className='slide'>
-                                    <img name="carouselImage2" src={pool} class="slide" alt=""/>
-                                </div>
+                            <div className='slide'>
+                                <img name="carouselImage2" src={pool} class="slide" alt=""/>
                             </div>
                         </div>
-
                     </div>
-
-
-                    <img src={whiteStars} id = "white-stars3"/>
-                    
-                    <div className='row justify-content-center wrapper-text offset-down3 mb-3' >
-
-                        <div className='col-10'>
-
-                            <p className='text-white font-poppins'>There are many ways to have fun in Snowdays23! There will be Skiing, Snowboarding, Snow-Volleyball, Swimming & many ohter Snow-Games!</p>
-
-                        </div>
-
-                     </div>
 
                 </div>
 
