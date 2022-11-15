b54558b2552e807e0e1a5e1c6c715a447d30fb12
Andrea Esposito <aespositox@gmail.com>
2022-11-15 17:38:57 +0100

    Attempted and abandoned carousel buttons (for now)

--- src/components/subcomponents/images-carousel.js
+++ src/components/subcomponents/images-carousel.js
@@ -45,6 +45,16 @@ export default function ImagesCarousel({images, intervalMs=1000}) {
             minHeight: "64vmin"
         }} id="carousel-inner">
             <div class="carousel-slide should-enter"></div>
+            {/* <button class="carousel-button left">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="white" class="bi bi-chevron-compact-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"/>
+                </svg>
+            </button>
+            <button class="carousel-button right">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="white" class="bi bi-chevron-compact-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
+                </svg>
+            </button> */}
         </div>
     );
 };
